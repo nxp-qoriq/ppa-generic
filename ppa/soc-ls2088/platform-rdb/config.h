2862e2c3a653104d370ef6c591d26144ac1af99a
bl4nkv01d <[york.sun@nxp.com]>
2017-03-01 11:44:22 -0600

    Add CONFIG_ARCH_* to gate errata macros
    Signed-off-by: bl4nkv01d <york.sun@nxp.com>
    Signed-off-by: rocketmon <rod.dorris@freescale.com>

--- ppa/soc-ls2088/platform-rdb/config.h
+++ ppa/soc-ls2088/platform-rdb/config.h
@@ -33,6 +33,7 @@
  */
 
 #define CONFIG_SYS_LSCH3
+#define CONFIG_ARCH_LS2088A
 #define TOP_OF_OCRAM		0x1801ffff      /* 128K */
 #define DDR_TEST_TABLE		(TOP_OF_OCRAM - 4096 * 3 + 1)
 #define TOP_OF_STACK		(DDR_TEST_TABLE - 1)
