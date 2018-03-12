ff7bb54fd74f1afda1304e191b66f0e91b2ad325
Bhaskar Upadhaya <Bhaskar.Upadhaya@nxp.com>
2018-03-12 13:19:56 +0530

    LS1012AFRWY: Remove white spaces
    Remove white spaces from ppa/soc-ls1012/platform-frwy/Makefile
    
    Signed-off-by: Bhaskar Upadhaya <Bhaskar.Upadhaya@nxp.com>

--- ppa/soc-ls1012/platform-frwy/plat.h
+++ ppa/soc-ls1012/platform-frwy/plat.h
@@ -1,7 +1,6 @@
 //-----------------------------------------------------------------------------
 //
-// Copyright (c) 2016, NXP Semiconductors 
-// Copyright 2017 NXP Semiconductors
+// Copyright 2018 NXP
 // 
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
--- ppa/soc-ls1012/platform-frwy/plat.mk
+++ ppa/soc-ls1012/platform-frwy/plat.mk
@@ -1,7 +1,6 @@
 #------------------------------------------------------------------------------
 # 
-# Copyright (C) 2015-2017 Freescale Semiconductor, Inc.
-# Copyright 2017-2018 NXP Semiconductors
+# Copyright 2018 NXP
 # 
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
